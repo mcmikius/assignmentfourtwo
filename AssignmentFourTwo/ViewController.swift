bf5a3dfdbe1f4d2480c219a236017dd1cb0e327a
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-17 09:03:28 +0300

    Initial Commit

---
+++ AssignmentFourTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentFourTwo
+//
+//  Created by Michail Bondarenko on 4/17/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

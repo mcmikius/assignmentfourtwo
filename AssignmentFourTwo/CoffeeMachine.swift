37fd389887e5490ff3b94fbe8193d2d9f21ede80
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-17 09:15:29 +0300

    Create two class files

---
+++ AssignmentFourTwo/CoffeeMachine.swift
@@ -0,0 +1,13 @@
+//
+//  CoffeeMachine.swift
+//  AssignmentFourTwo
+//
+//  Created by Michail Bondarenko on 4/17/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeMachine: NSObject {
+
+}

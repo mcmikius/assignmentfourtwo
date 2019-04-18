75ef96aede0dce1884327c69713d698f98d753be
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-18 09:11:39 +0300

    add properties and base  function into class

--- AssignmentFourTwo/CoffeeMachine.swift
+++ AssignmentFourTwo/CoffeeMachine.swift
@@ -7,7 +7,25 @@
 //
 
 import UIKit
+import Foundation
 
 class CoffeeMachine: NSObject {
+    var amountOfCoffee = 0
+    var amountOfWater = 0
+    var amountOfMilk = 0
+    var tray = 0
+    
+    func addCoffee() {
+        
+    }
+    func addWater() {
+        
+    }
+    func addMilk() {
+        
+    }
+    func clearTray() {
+        
+    }
 
 }
